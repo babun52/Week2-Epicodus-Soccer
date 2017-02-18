38f46611c706320c9914074893063cdc887cae81
James Lee <babun5209@gmail.com>
2017-02-18 14:05:09 -0800

    Implement Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'landing#index'
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 end
