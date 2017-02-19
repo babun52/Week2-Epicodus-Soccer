26faeecbef056c0299754ef98a9c2d581ed1bfcd
James Lee <babun5209@gmail.com>
2017-02-18 17:30:27 -0800

    Add gem 'twitter-bootstrap-rails' and gem 'devise-bootstrap-views' for styling the authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'landing#index'
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, path_names: {sign_in: "login", sign_up: "signup",sign_out: "logout"}
 end
